2894c31f889fe966b68f8c7992858af57c8cfea7
Atif Khan <atif.q.khan@gmail.com>
2013-05-21 15:45:49 +1000

    Added mod_rewrite and configured blueplains recipe for correct src_path

--- cookbooks-e1/projects/recipes/blueplains.rb
+++ cookbooks-e1/projects/recipes/blueplains.rb
@@ -31,14 +31,14 @@ if !!node['blueplains']['db_setup'] == true
 	database 'mahlab_blueplains' do
 	  connection conn_info
 		provider Chef::Provider::Database::Mysql
-	  sql { ::File.open("#{node['blueplains']['src_root']}/sql/db-model.sql").read }
+	  sql { ::File.open("#{node['blueplains']['src_path']}/sql/db-model.sql").read }
 	  action :query
 	end
 
 		database 'mahlab_blueplains' do
 	  connection conn_info
 		provider Chef::Provider::Database::Mysql
-	  sql { ::File.open("#{node['blueplains']['src_root']}/sql/db-data.sql").read }
+	  sql { ::File.open("#{node['blueplains']['src_path']}/sql/db-data.sql").read }
 	  action :query
 	end
 
@@ -47,8 +47,8 @@ end
 web_app "insuranceandrisk" do
 	server_name node['blueplains']['irp_domain']
 	server_aliases [ node['fqdn'] ]
-	docroot "#{node['blueplains']['src_root']}/#{node['blueplains']['irp_docroot']}"
-
+	docroot "#{node['blueplains']['src_path']}/#{node['blueplains']['irp_docroot']}"
+  	allow_override "FileInfo"
 	# this specifies which cookbook includes the basic web_app.conf template
 	# should a project require a more specific template, commenting this line
 	# and creating the template in this 'project' cookbook should do, using:
@@ -60,8 +60,8 @@ end
 web_app "probus" do
 	server_name node['blueplains']['pb_domain']
 	server_aliases [ node['fqdn'] ]
-	docroot "#{node['blueplains']['src_root']}/#{node['blueplains']['pb_docroot']}"
-
+	docroot "#{node['blueplains']['src_path']}/#{node['blueplains']['pb_docroot']}"
+  	allow_override "FileInfo"
 	# this specifies which cookbook includes the basic web_app.conf template
 	# should a project require a more specific template, commenting this line
 	# and creating the template in this 'project' cookbook should do, using:
@@ -73,8 +73,8 @@ end
 web_app "blueplains-cms" do
 	server_name node['blueplains']['cms_domain']
 	server_aliases [ node['fqdn'] ]
-	docroot "#{node['blueplains']['src_root']}/#{node['blueplains']['cms_docroot']}"
-
+	docroot "#{node['blueplains']['src_path']}/#{node['blueplains']['cms_docroot']}"
+  	allow_override "FileInfo"
 	# this specifies which cookbook includes the basic web_app.conf template
 	# should a project require a more specific template, commenting this line
 	# and creating the template in this 'project' cookbook should do, using:
@@ -86,8 +86,9 @@ end
 web_app "blueplains-microsite" do
 	server_name node['blueplains']['microsite_domain']
 	server_aliases [ node['fqdn'] ]
-	docroot "#{node['blueplains']['src_root']}/#{node['blueplains']['microsite_docroot']}"
-
+	docroot "#{node['blueplains']['src_path']}/#{node['blueplains']['microsite_docroot']}"
+  	allow_override "FileInfo"
+  
 	# this specifies which cookbook includes the basic web_app.conf template
 	# should a project require a more specific template, commenting this line
 	# and creating the template in this 'project' cookbook should do, using:
@@ -96,15 +97,15 @@ web_app "blueplains-microsite" do
 end
 
 
-template "#{node['blueplains']['src_root']}/blueplains/app-public/config/core.php" do
+template "#{node['blueplains']['src_path']}/app-public/config/core.php" do
   source "blueplains/app-public/config/core.php.erb"
   mode 00644
 end
-template "#{node['blueplains']['src_root']}/blueplains/app-private/config/core.php" do
+template "#{node['blueplains']['src_path']}/app-private/config/core.php" do
   source "blueplains/app-private/config/core.php.erb"
   mode 00644
 end
-template "#{node['blueplains']['src_root']}/blueplains/app-microsite/config/core.php" do
+template "#{node['blueplains']['src_path']}/app-microsite/config/core.php" do
   source "blueplains/app-microsite/config/core.php.erb"
   mode 00644
 end
