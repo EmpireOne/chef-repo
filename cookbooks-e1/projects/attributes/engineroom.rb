bec781839a7d868efb53cc7580bffb984f030db0
Atif Khan <atif.q.khan@gmail.com>
2013-06-25 16:06:50 +1000

    test push

--- cookbooks-e1/projects/attributes/engineroom.rb
+++ cookbooks-e1/projects/attributes/engineroom.rb
@@ -1,5 +1,5 @@
 default['rvm']['default_ruby']      = "ruby-1.9.3-p392"
 default['rvm']['user_default_ruby'] = "ruby-1.9.3-p392"
 default['nginx']['version'] = "1.4.1"
-default['nginx']['passenger']['version'] = "4.0.2"
+default['nginx']['passenger']['version'] = "4.0.1"
 default['engineroom']['db_setup'] = true
